68bc533865d2e7616fc9b3b4611eebc78a067b08
Simon Johansson <simon.johansson@screeninteraction.com>
2015-02-17 17:58:31 +0100

    Changes to the test globals
    Added two helper functions getHTML and generateHTML

--- test/globals.js
+++ test/globals.js
@@ -1,5 +1,16 @@
-global.expect = require("chai").expect;
-global.jade = require('jade');
-global.jquery = require('fs').readFileSync(__dirname + "/jquery.js", "utf-8");
-global.jsdom = require('mocha-jsdom');
-global.baseDir = __dirname + '/..';
+expect = require("chai").expect;
+jade = require('jade');
+jquery = require('fs').readFileSync(__dirname + "/jquery.js", "utf-8");
+jsdom = require('mocha-jsdom');
+baseDir = __dirname + '/..';
+
+getHTML = function (selector) {
+  return $(selector).html();
+};
+
+generateHTML = function(jadeCode) {
+  return jade.render(jadeCode, {
+    basedir: baseDir,
+    pretty: true
+  });
+};
